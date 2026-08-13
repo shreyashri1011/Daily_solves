6bc281334650bd6cb1e0756113ddf737e3d0f9a2
shreyashri1011 <shreyashriphatak@gmail.com>
2026-08-13 11:20:19 +0530

    Time: 3 ms (44.39%) | Memory: 14.2 MB (65.74%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -1,5 +1,9 @@
-from collections import deque
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def hasPathSum(self, root, targetSum):
         """
@@ -7,28 +11,15 @@ class Solution(object):
         :type targetSum: int
         :rtype: bool
         """
-
         if not root:
             return False
-
-        queue = deque([(root, root.val)])
-
-        while queue:
-
-            curr, val = queue.popleft()
-
-            # Check if current node is a leaf
-            if not curr.left and not curr.right:
-
-                if val == targetSum:
-                    return True
-
-            # Add left child
-            if curr.left:
-                queue.append((curr.left, val + curr.left.val))
-
-            # Add right child
+        stack=[(root,root.val)]
+        while stack:
+            curr,val=stack.pop()
+            if not curr.left and not curr.right and val==targetSum:
+                return True
             if curr.right:
-                queue.append((curr.right, val + curr.right.val))
-
+                stack.append((curr.right,val+curr.right.val))
+            if curr.left:
+                stack.append((curr.left,val+curr.left.val))
         return False
