838df1ab1f46b440650eebf19b656a8815e43058
shreyashri1011 <shreyashriphatak@gmail.com>
2026-08-27 16:59:34 +0530

    Time: 15 ms (32.99%) | Memory: 14.2 MB (56.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,16 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        occur = {}
-        for num in nums:
-            occur[num] = occur.get(num, 0) + 1
-
-        bucket = [[] for _ in range(n+1)]
-
-        for key in occur:
-            bucket[occur[key]].append(key)
-
-        res = []
-        for i in range(n, 0, -1):
-            if len(res) >= k:
-                break
-            for val in bucket[i]:
-                res.append(val)
-        return res
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count=Counter(nums)
+        heap=[]
+        for num,freq in count.items():
+            if len(heap) < k :
+                heapq.heappush(heap,(freq,num))
+            elif freq>heap[0][0]:
+                heapq.heapreplace(heap,(freq,num))
+        top_k=[num for freq , num in heap]
+        return top_k
