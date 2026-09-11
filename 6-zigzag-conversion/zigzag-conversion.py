85cd47e0ccacb84ddec2c5d0e969ca347bef95fd
shreyashri1011 <shreyashriphatak@gmail.com>
2026-09-11 11:54:35 +0530

    Time: 29 ms (7.58%) | Memory: 12.4 MB (56.63%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -5,11 +5,12 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        if numRows == 1: return s
-        a=""
-        for i in range(numRows):
-            for j in range(i,len(s),2*(numRows-1)):
-                a+=s[j]
-                if(i>0 and i<numRows-1 and j+2*(numRows-1)-2*i < len(s)):
-                    a+=s[j+2*(numRows-1)-2*i]
-        return a
+        if numRows==1: return s
+        res=""
+        for r in range(numRows):
+            increment=2*(numRows -1)
+            for i in range(r,len(s),increment):
+                res+=s[i]
+                if ( r>0 and r<numRows-1 and i+increment-2*r < len(s)):
+                    res+= s[i+increment-2*r]
+        return res
