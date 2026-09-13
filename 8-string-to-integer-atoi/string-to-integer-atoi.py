a1bc6b92149c531d9083e9ce472c348f95eaf6ec
shreyashri1011 <shreyashriphatak@gmail.com>
2026-09-13 10:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (59.16%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -4,37 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        i = 0
-        n = len(s)
-        
-        # 1. Skip leading whitespace
-        while i < n and s[i] == ' ':
-            i += 1
-            
-        if i == n:
+        i=0
+        n=len(s)
+        while i < n and s[i]==' ':
+            i+=1
+        if i==n:
             return 0
-        
-        # 2. Determine sign
-        sign = 1
-        if s[i] == '-':
-            sign = -1
-            i += 1
-        elif s[i] == '+':
-            i += 1
-            
-        # 3. Read digits & check boundaries
+        sign=1
+        if s[i]=='-':
+            sign=-1
+            i+=1
+        elif s[i]=='+':
+            i+=1
         result = 0
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-        
+        INT_MAX=2**31-1
+        INT_MIN= -2**31
         while i < n and s[i].isdigit():
-            digit = int(s[i])
-            
-            # Check for potential overflow before updating result
+            digit=int(s[i])
             if (result > INT_MAX // 10) or (result == INT_MAX // 10 and digit > 7):
-                return INT_MAX if sign == 1 else INT_MIN
-            
-            result = result * 10 + digit
-            i += 1
-            
-        return sign * result
+                return INT_MAX if sign==1 else INT_MIN
+            result = result*10 + digit
+            i+=1
+        return sign*result
