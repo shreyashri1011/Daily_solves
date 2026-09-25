1e345b4c780f1e288484f42c7a9a9e10c99464f7
shreyashri1011 <shreyashriphatak@gmail.com>
2026-09-25 21:30:20 +0530

    Time: 2 ms (7.77%) | Memory: 12.4 MB (59.34%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -7,29 +7,21 @@ class Solution(object):
         if not digits:
             return []
             
-        # Dictionary mapping digits to letters
         phone_map = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
         
         result = []
-
-        # Helper recursive function
-        def solve(index, current_string):
-            # Base Case: When the combined string length matches input length
-            if index == len(digits):
+        def solve(index,current_string):
+            if index==len(digits):
                 result.append(current_string)
                 return
-            
-            # Fetch letters for current digit from dictionary
-            current_digit = digits[index]
-            letters = phone_map[current_digit]
-            
-            # Recurse for every possible letter
-            for letter in letters:
-                solve(index + 1, current_string + letter)
+            current_digit=digits[index]
+            letters=phone_map[current_digit]
 
-        # Start recursion at index 0 with an empty string
-        solve(0, "")
+            for letter in letters:
+                solve(index+1,current_string+letter)
+        solve(0,"")
         return result
+        
